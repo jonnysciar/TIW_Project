938981f0cc06e573864aba7ab7efc8f3202f4fdd
Jonathan Sciarrabba <sciarrabba.j00@gmail.com>
2022-07-21 14:59:12 +0200

    first setup

---
+++ html_pure/src/main/java/it/jonnysciar/html_pure/HelloServlet.java
@@ -0,0 +1,32 @@
+package it.jonnysciar.html_pure;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@WebServlet(name = "helloServlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() {
+        message = "Ciao Mondo!";
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ html_pure/src/main/java/it/jonnysciar/html_pure/TestServlet.java
@@ -0,0 +1,22 @@
+package it.jonnysciar.html_pure;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "testServlet")
+public class TestServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "ciao a tutti!" + "</h1>");
+        out.println("</body></html>");
+    }
+}
